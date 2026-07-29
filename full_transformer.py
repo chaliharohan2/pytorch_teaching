Finished the SFT and training runs. Bug fix: Earlier had not shifted the inputs and labels to ensure causal attention. Fixed now. Moving on to making a tokenizer.

full_transformer.py
@@ -124,22 +124,26 @@ class GPT(nn.Module):
 
         return x
 
-    def generate(self, max_tokens, token_to_char, max_context_length, start_token=1):
+    def generate(self, max_tokens, token_to_char, max_context_length, start_token=1, greedy=False):
         with torch.no_grad():
             sequence = torch.tensor([start_token], device=device).unsqueeze(0) if type(start_token) == int else torch.tensor(start_token, device=device).unsqueeze(0)
             for _ in range(max_tokens):
                 preds = self(sequence[:, -max_context_length:]) # 1, C, vocab_size - making sure only the max context length tokens go in
                 preds = preds[:, -1, :] # only interested in the last token vals - 1, vocab_size
                 preds = self.softmax(preds) # get probabilities of each word in vocab - 1, vocab_size
-                pred = torch.multinomial(preds, num_samples=1)
+                # if greedy decoding then take most likely token only
+                if greedy:
+                    pred = torch.argmax(preds, dim=-1).unsqueeze(0)
+                else:
+                    pred = torch.multinomial(preds, num_samples=1)
                 pred_token = token_to_char.get(pred.item())
                 print(pred_token, end="", flush=True)
 
                 sequence = torch.cat([sequence, pred], dim=1)
 
-    def generate_assistant_response(self, question, tokenizer, max_tokens, token_to_char, max_context_length):
+    def generate_assistant_response(self, question, tokenizer, max_tokens, token_to_char, max_context_length, greedy=False):
         final_input = [tokenizer.get("<user>"), tokenizer.get("\n"), *[tokenizer.get(char) for char in question], tokenizer.get("\n"), tokenizer.get("<assistant>"), tokenizer.get("\n")]
-        self.generate(max_tokens=max_tokens, token_to_char=token_to_char, max_context_length=max_context_length, start_token=final_input)
+        self.generate(max_tokens=max_tokens, token_to_char=token_to_char, max_context_length=max_context_length, start_token=final_input, greedy=greedy)
 
 if DATASET_TO_USE == "tiny_shakespeare":
     with open("/home/nz-dgx-spark-01/Documents/Nyalazone/pytorch_testing/shakespeare_dataset.txt", mode="r") as f:
@@ -163,12 +167,13 @@ encode = lambda x: [tokenizer.get(c) for c in x]
 
 vocab_size = len(raw_unique_chars)
 
-# Training components initialized
-model = GPT(vocab_size=vocab_size, max_context_length=MAX_LENGTH, embedding_size=EMBEDDING_SIZE, num_heads=NUM_HEADS, head_size=HEAD_SIZE, num_decoder_layers=NUM_DECODER_BLOCKS)
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.AdamW(params=model.parameters(), lr=3e-4)
-
 if __name__ == "__main__":
+
+    # Training components initialized
+    model = GPT(vocab_size=vocab_size, max_context_length=MAX_LENGTH, embedding_size=EMBEDDING_SIZE, num_heads=NUM_HEADS, head_size=HEAD_SIZE, num_decoder_layers=NUM_DECODER_BLOCKS)
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.AdamW(params=model.parameters(), lr=3e-4)
+
     j = 0
     raw_dataset = []
 

sft.py
@@ -76,7 +76,10 @@ def tokenization_helper(input_text: str, output_text: str, tokenizer: dict):
     output_tok_seq = _tokenize(seq_to_tokenize=output_seq, tokenizer=tokenizer)
 
     final_tokenized_seq = input_tok_seq + output_tok_seq
-    batch_labels = [-100]*len(input_tok_seq) + output_tok_seq
+
+    # shifting labels to the right by 1 so that each token predicts the next token, not the same one
+    final_tokenized_seq = final_tokenized_seq[:-1] 
+    batch_labels = [-100]*(len(input_tok_seq)-1) + output_tok_seq
 
     assert len(final_tokenized_seq) == len(batch_labels)
 
@@ -138,7 +141,7 @@ if __name__ == "__main__":
 
     # load training components
     loss_fn = nn.CrossEntropyLoss(ignore_index=-100) # to ignore padded tokens
-    optimizer = torch.optim.AdamW(params=model.parameters(), lr=3e-4)
+    optimizer = torch.optim.AdamW(params=model.parameters(), lr=1e-5)
 
     raw_dataset = []
 