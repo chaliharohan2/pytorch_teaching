126e32f2e475df675c52601f21e761718717d4de
Rohan Chaliha <chaliharohan2@gmail.com>
2026-08-03 18:30:02 +0530

    Trying to make the decode function for the tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,4 @@
-def get_merges(encoded_text: list):
+def encode(encoded_text: list):
     tokens = list(encoded_text)
     merges = {}
     j = 256
@@ -22,6 +22,21 @@ def get_merges(encoded_text: list):
 
     return merges, tokens
 
+def decode(tokens: list, merges: dict):
+    # take the raw bytes for tokens from 0 to 255 which is range for utf-8
+    vocab = {idx: bytes([idx]) for idx in range(256)}
+
+    # add the bytes for merged tokens (which is sum of bytes of merged pair)
+    for merge, val in merges.items():
+        vocab[val] = bytes([merge[0]]) + bytes([merge[1]])
+
+    input_bytes = [vocab[tok] for tok in tokens]
+
+    decoded_str = [inp.decode() for inp in input_bytes]
+    return decoded_str
+
+    
+
 text = "In response to this incident, we began a large-scale retrospective review of our own cybersecurity evaluations. In particular, we looked for evidence that Claude—like the OpenAI models that accessed Hugging Face—was able to access the internet from within testing environments that should have been sealed off. 😀 😃 😄 😁 🥳"
 # text = "Hello world, some apple in town."
 encoded_text = list(text.encode("utf-8"))
@@ -35,7 +50,8 @@ for pair in zip(encoded_text[:-1], encoded_text[1:]):
 top_pair = max(pair_counts, key=pair_counts.get)
 # print(top_pair, f" / \"{chr(top_pair[0])} {chr(top_pair[1])}\": ", pair_counts[top_pair])
 
-merges, tokens = get_merges(encoded_text)
+merges, tokens = encode(encoded_text)
 print(merges)
 print(tokens)  
 print("After tokenizing length: ", len(tokens))       
+
