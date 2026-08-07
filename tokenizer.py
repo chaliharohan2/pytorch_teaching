71503a3a55a6a2bf8dc70105c294557b811d5a20
rohan <chaliharohan2@gmail.com>
2026-08-07 17:18:13 +0530

    Finished BPE tokenizer encoder and decoder basic

--- tokenizer.py
+++ tokenizer.py
@@ -28,16 +28,17 @@ def decode(tokens: list, merges: dict):
 
     # add the bytes for merged tokens (which is sum of bytes of merged pair)
     for merge, val in merges.items():
-        vocab[val] = bytes([merge[0]]) + bytes([merge[1]])
+        vocab[val] = vocab[merge[0]] + vocab[merge[1]]
 
     input_bytes = [vocab[tok] for tok in tokens]
 
-    decoded_str = [inp.decode() for inp in input_bytes]
+    decoded_str = [inp.decode(encoding="utf-8", errors="replace") for inp in input_bytes]
     return decoded_str
 
     
-
-text = "In response to this incident, we began a large-scale retrospective review of our own cybersecurity evaluations. In particular, we looked for evidence that Claude—like the OpenAI models that accessed Hugging Face—was able to access the internet from within testing environments that should have been sealed off. 😀 😃 😄 😁 🥳"
+with open(file="/home/rohan/Nyalazone/pytorch_testing/shakespeare_dataset.txt", mode="r") as f:
+    text = f.read()
+# text = "In response to this incident, we began a large-scale retrospective review of our own cybersecurity evaluations. In particular, we looked for evidence that Claude—like the OpenAI models that accessed Hugging Face—was able to access the internet from within testing environments that should have been sealed off."
 # text = "Hello world, some apple in town."
 encoded_text = list(text.encode("utf-8"))
 print("Original length: ", len(encoded_text))
@@ -55,6 +56,7 @@ print(merges)
 print(tokens)  
 print("After tokenizing length: ", len(tokens))     
 
-tokens_to_decode = [72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 44, 32, 115, 111, 109, 256, 97, 112, 112, 108, 256, 105, 110, 32, 116, 111, 119, 110, 46]
-print("Decoding: ", "".join(decode(tokens=tokens_to_decode, merges=merges)))
-print("Original text: ", text)
+# tokens_to_decode = [276, 114, 261, 112, 277, 280, 281, 259, 262, 99, 283, 116, 285, 267, 103, 97, 258, 97, 32, 286, 103, 101, 45, 115, 99, 269, 287, 288, 289, 115, 112, 101, 99, 290, 118, 287, 270, 105, 101, 119, 291, 263, 292, 263, 119, 258, 99, 121, 267, 114, 271, 99, 292, 105, 116, 121, 293, 269, 117, 264, 105, 277, 46, 32, 276, 112, 97, 114, 290, 99, 117, 286, 285, 108, 111, 111, 107, 101, 272, 102, 111, 114, 293, 283, 99, 256, 274, 67, 268, 117, 294, 296, 108, 105, 107, 256, 297, 79, 112, 260, 65, 73, 32, 109, 111, 294, 108, 298, 300, 271, 272, 72, 117, 103, 103, 302, 70, 275, 101, 296, 119, 97, 259, 97, 98, 108, 280, 300, 259, 297, 262, 116, 101, 114, 110, 288, 32, 102, 289, 109, 266, 105, 281, 258, 116, 261, 116, 302, 260, 118, 105, 114, 265, 109, 260, 116, 298, 115, 104, 111, 117, 108, 272, 104, 97, 118, 256, 267, 101, 258, 271, 269, 101, 100, 291, 102, 46]
+# print("Decoding: ", new_text := "".join(decode(tokens=tokens_to_decode, merges=merges)))
+# print("Original text: ", text)
+# print(text == new_text)
